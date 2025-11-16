60c0d12c8ffe6334ec8184de835d32f1b19c886f
Developer <dev@example.com>
2025-11-17 03:48:20 +0530

    init project

---
+++ app/layout.js
@@ -0,0 +1,38 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import ThemeProviderWrapper from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Welth",
+  description: "One stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProviderWrapper>
+            <Header />
+            <main className="min-h-screen bg-white dark:bg-gray-900">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-blue-50 dark:bg-gray-800 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-600 dark:text-gray-300">
+                <p>Made with 💗 by RoadsideCoder</p>
+              </div>
+            </footer>
+          </ThemeProviderWrapper>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
